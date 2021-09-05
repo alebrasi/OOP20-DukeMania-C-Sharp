f0c14b06b493e7833d03d3178e5f2e089c783f23
alebrasi <alessandro.brasini1@gmail.com>
2021-09-05 11:21:01 +0200

    Test class refactoring

--- AlessandroBrasini/Test/Test.cs
+++ AlessandroBrasini/Test/Test.cs
@@ -9,34 +9,56 @@ namespace Test
     [TestClass]
     public class Test
     {
-        IStorage _storage = new StorageFactory().GetConfigurationStorage();
+        private static IStorage _storage;
+        private static string _path;
+        private static string _dirSeparator;
+
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
+        {
+            _storage = new StorageFactory().GetConfigurationStorage();
+            _path = _storage.GetAsFile("").Path;
+            _dirSeparator = Path.DirectorySeparatorChar.ToString();
+        }
 
         [TestMethod]
-        public void TestMethod1()
-        {   
-            string path = _storage.GetAsFile("").Path;
-            string dirSeparator = Path.DirectorySeparatorChar.ToString();
-
+        public void TestCreateDirectoryRecursively()
+        {  
             _storage.CreateDirectoryRecursively("test/test2");
-            Assert.IsTrue(Directory.Exists(path + dirSeparator + "test" + dirSeparator + "test2"));
+            Assert.IsTrue(Directory.Exists(_path + _dirSeparator + "test" + _dirSeparator + "test2"));
+        }
 
+        [TestMethod]
+        public void TestCreateFileIfNotExists()
+        {
             _storage.CreateFileIfNotExists("testFile");
-            Assert.IsTrue(File.Exists(path + dirSeparator + "testFile"));
+            Assert.IsTrue(File.Exists(_path + _dirSeparator + "testFile"));
+        }
 
+        [TestMethod]
+        public void TestWriteStringOnFile()
+        {
             _storage.WriteStringOnFile("testFile", "test");
             Assert.AreEqual(_storage.ReadFileAsString("testFile"), "test");
-            
+        }
+
+        [TestMethod]
+        public void TestReadBytesFromFile()
+        {
             _storage.CreateFileIfNotExists("testFile2");
             byte[] payload = {84, 104, 101, 32, 71, 97, 109, 101};
-            File.WriteAllBytes(path + dirSeparator + "testFile2", payload);
+            File.WriteAllBytes(_path + _dirSeparator + "testFile2", payload);
             CollectionAssert.AreEqual(_storage.ReadFileAsByte("testFile2"), payload);
+        }
 
-            //Cleanup
-            Directory.Delete(path + dirSeparator + "test" + dirSeparator + "test2");
-            Directory.Delete(path + dirSeparator + "test");
-            File.Delete(path + dirSeparator + "testFile");
-            File.Delete(path + dirSeparator + "testFile2");
-            Directory.Delete(path);
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            Directory.Delete(_path + _dirSeparator + "test" + _dirSeparator + "test2");
+            Directory.Delete(_path + _dirSeparator + "test");
+            File.Delete(_path + _dirSeparator + "testFile");
+            File.Delete(_path + _dirSeparator + "testFile2");
+            Directory.Delete(_path);
         }
 
     }
